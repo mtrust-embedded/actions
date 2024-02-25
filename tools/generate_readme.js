cf5d836013be152b28edf9ea6a97bbb893a7c234
Chris <chris@koch.codes>
2024-02-25 18:44:00 +0100

    .

--- tools/generate_readme.js
+++ tools/generate_readme.js
@@ -1,13 +1,7 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const fs = require('fs');
-        
 // Your Node.js script logic goes here
 // For example, reading a JSON file
 const filePath = 'library.json';
 try {
-
-  const github_pages_url = core.getInput('gh_pages_url');
 
   const jsonData = fs.readFileSync(filePath, 'utf8');
   const data = JSON.parse(jsonData);
