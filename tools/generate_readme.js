c0d9d9cc94dc54ba9dc1105f17fe6602c33ce0e7
Chris <chris@koch.codes>
2024-02-25 18:48:07 +0100

    .

--- tools/generate_readme.js
+++ tools/generate_readme.js
@@ -1,11 +1,20 @@
-// Your Node.js script logic goes here
-// For example, reading a JSON file
-const filePath = 'library.json';
-try {
+const fs = require('fs');
 
+const url = process.argv[2];
+
+if (!url) {
+  console.error('Please provide the github pages url.');
+  process.exit(1);
+}
+const filePath = 'library.json';
+
+try {
   const jsonData = fs.readFileSync(filePath, 'utf8');
   const data = JSON.parse(jsonData);
   console.log(data);
+
+  fs.writeFileSync('README.md', `[Pages](${url})   # Data from JSON File\n\n\`\`\`\n${JSON.stringify(data, null, 2)}\n\`\`\`\n`, 'utf8');
+  
 } catch (err) {
   console.error('Error reading JSON file:', err);
 }
