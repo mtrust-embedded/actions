8710e8393db33c98a13f953422d19a6d81791dcf
Chris <chris@koch.codes>
2024-02-25 20:52:27 +0100

    .

--- tools/generate_readme.js
+++ tools/generate_readme.js
@@ -17,7 +17,10 @@ try {
   const dep_badges = fs.readFileSync('docs/dep-badges.md', 'utf8');
   const coverage = fs.readFileSync('docs/coverage.txt', 'utf8');
 
-  fs.writeFileSync('Readme.md', '- [Coverage Report](' + url + '/coverage/html)\n   - [Doxygen](' + url + '/doxygen)\n' + ci_badges + '\n   ' + dep_badges + '\n   ### Coverage\n   ```' + coverage + '```', 'utf8');
+  const doxygen_link = `[![Doxygen](https://img.shields.io/badge/Doxygen-Code_Documentation-0F69AF?logo=doxygen&logoColor=AAA)](${url}/doxygen/html)`;
+  const code_coverage = `[![GCov](https://img.shields.io/badge/gcov-Coverage_Report-0F69AF?logo=coveralls&logoColor=AAA)](${url}/coverage)`;
+
+  fs.writeFileSync('Readme.md', doxygen_link + code_coverage + '\n   ' + ci_badges + '\n   ' + dep_badges + '\n   ### Coverage\n   ' + coverage + '', 'utf8');
   
 } catch (err) {
   console.error('Error reading JSON file:', err);
